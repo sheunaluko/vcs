0d6e36a9e0b4d5f11df316934b64231315ae7300
Sheun Aluko <oluwa@DN0a1ea9cb.SUNet>
2020-02-29 23:21:37 -0800

    +typescript entities

--- src/nodejs/typescript/src/entities/dist/entities/index.js
+++
@@ -1,9 +0,0 @@
-const { Entity } = require("./entity");
-const res = require("./resources/index");
-//const ops = require("./operations/index.js") 
-module.exports = {
-    res,
-    //ops, 
-    Entity,
-};
-//# sourceMappingURL=index.js.map
